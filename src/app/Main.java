f7723eb24c88db231aa8e51c3ea7104f17c122fc
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-08-31 23:47:48 +0300

    Solution Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
+import app.viewers.PdfViewer;
+
 public class Main {
     public static void main(String[] args) {
+        PdfViewer pdfViewer = new PdfViewer();
 
+        pdfViewer.view("pdf", "Data structure and Algorithms in JAVA.pdf");
+        pdfViewer.view("png", "Patterns in Java.png");
+        pdfViewer.view("jpg", "Collections types in JAVA.jpg");
+        pdfViewer.view("cdr", "Diagram.cdr");
     }
 }
